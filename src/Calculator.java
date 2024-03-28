dee7ed47ca77f094d12a71e30375ebe99bec65fe
Mateus Felipe <brasil.mateus748@gmail.com>
2024-03-28 03:37:12 -0300

    Add conditionals that controls the flow of the operations and methods that represents each one of them

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,34 @@
 public class Calculator {
     public static void main(String[] args) {
+        int a = Integer.parseInt(args[1]);
+        int b = Integer.parseInt(args[2]);
 
-    }
+        if (args[0].equals("sum")) {
+            sum(a, b);
+        } else if (args[0].equals("subtract")) {
+            subtract(a, b);
+        } else if (args[0].equals(("multiply"))) {
+            multiply(a, b);
+        } else if (args[0].equals("divide")) {
+            divide(a, b);
+        } else {
+            System.out.println("Invalid input!");
+        };
+    };
 
+    static void sum(int a, int b) {
+        System.out.println(a + b);
+    };
 
-}
+    static void subtract(int a, int b) {
+        System.out.println(a - b);
+    };
+
+    static void multiply(int a, int b) {
+        System.out.println(a * b);
+    };
+
+    static void divide(int a, int b) {
+        System.out.println(a / b);
+    };
+};
